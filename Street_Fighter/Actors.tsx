2e9d1a1a60f371934b053fb5900c5b5f6ca8e270
Malik <31506138+msmalik681@users.noreply.github.com>
2022-10-03 10:32:30 +0100

    initial commit
    starting commit

---
+++ Street_Fighter/Actors.tsx
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="actors" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="actors.png" width="256" height="256" />
+  <tile id="0" />
+  <tile id="1">
+    <properties>
+      <property name="Name" value="001_fireball" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="007_retsu_r" type="string" />
+    </properties>
+  </tile>
+  <tile id="3" />
+  <tile id="4">
+    <properties>
+      <property name="Name" value="002_ryu_r" type="string" />
+    </properties>
+  </tile>
+  <tile id="5" />
+  <tile id="6">
+    <properties>
+      <property name="Name" value="004_ryu_duck_r" type="string" />
+    </properties>
+  </tile>
+  <tile id="7" />
+  <tile id="8">
+    <properties>
+      <property name="Name" value="006_ryu_tatsu" type="string" />
+    </properties>
+  </tile>
+  <tile id="9">
+    <properties>
+      <property name="Name" value="003_ryu_l" type="string" />
+    </properties>
+  </tile>
+  <tile id="10">
+    <properties>
+      <property name="Name" value="005_ryu_duck_l" type="string" />
+    </properties>
+  </tile>
+  <tile id="11">
+    <properties>
+      <property name="Name" value="008_retsu_l" type="string" />
+    </properties>
+  </tile>
+</tileset>
---
+++ Street_Fighter/RUN (copy).sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd "/home/malik/Documents/street fighter amiga/scorpion-editor/Editor"
+#export WINEPREFIX="$HOME/.wine32"
+wine "Scorpion Editor - 2022.7.exe"
+#xrandr --output HDMI-0 --mode 1680x1050
