3625212a9831d614e1f3999128be13c2ea5e7abd
Malik <31506138+msmalik681@users.noreply.github.com>
2025-11-30 16:33:30 +0000

    fix
    adding back files that were deleted.

---
+++ Street_Fighter/Actors.tsx
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="actors" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="actors.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="tiger_fireball" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="001_fireball" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="004_ryu" type="string" />
+    </properties>
+  </tile>
+  <tile id="3" />
+  <tile id="4">
+    <properties>
+      <property name="Name" value="005_ryu_duck" type="string" />
+    </properties>
+  </tile>
+  <tile id="5" />
+  <tile id="6">
+    <properties>
+      <property name="Name" value="002_camera" type="string" />
+    </properties>
+  </tile>
+  <tile id="7">
+    <properties>
+      <property name="Name" value="003_flash" type="string" />
+    </properties>
+  </tile>
+  <tile id="8">
+    <properties>
+      <property name="Name" value="006_retsu" type="string" />
+    </properties>
+  </tile>
+  <tile id="9" />
+  <tile id="10" />
+  <tile id="11" />
+  <tile id="12">
+    <properties>
+      <property name="Name" value="007_geki" type="string" />
+    </properties>
+  </tile>
+  <tile id="13">
+    <properties>
+      <property name="Name" value="008_joe" type="string" />
+    </properties>
+  </tile>
+  <tile id="14">
+    <properties>
+      <property name="Name" value="009_mike" type="string" />
+    </properties>
+  </tile>
+  <tile id="15">
+    <properties>
+      <property name="Name" value="010_lee" type="string" />
+    </properties>
+  </tile>
+  <tile id="16">
+    <properties>
+      <property name="Name" value="011_gen" type="string" />
+    </properties>
+  </tile>
+  <tile id="17">
+    <properties>
+      <property name="Name" value="012_birdie" type="string" />
+    </properties>
+  </tile>
+  <tile id="18">
+    <properties>
+      <property name="Name" value="013_eagle" type="string" />
+    </properties>
+  </tile>
+  <tile id="19">
+    <properties>
+      <property name="Name" value="014_adon" type="string" />
+    </properties>
+  </tile>
+  <tile id="20">
+    <properties>
+      <property name="Name" value="015_sagat" type="string" />
+    </properties>
+  </tile>
+</tileset>
